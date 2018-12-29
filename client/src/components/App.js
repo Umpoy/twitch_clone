920098975e597a8d71514f1e81d9049e0f77cb57
Umpoy <iumpoy@yahoo.com>
2018-12-28 20:06:35 -0800

    incorporated react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Home = () => {
+    return <div>Hello World</div>
+}
+
+const Test = () => {
+    return <div>Testing router...</div>
+}
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/test" component={Test} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
