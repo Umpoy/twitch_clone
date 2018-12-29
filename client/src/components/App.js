dca5890161b8d7805680d21fd76ccee2662a5588
Umpoy <iumpoy@yahoo.com>
2018-12-28 20:36:33 -0800

    start of header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,12 +5,14 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header'
 
 
 
 const App = () => {
     return (
         <div class="app">
+        <Header/>
             <BrowserRouter>
                 <div>
                     <Route path="/" exact component={StreamList} />
