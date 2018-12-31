82d65e3fb5e1296015589cc5f9ea16aea0c04c2c
Umpoy <iumpoy@yahoo.com>
2018-12-31 11:08:53 -0800

    renders weither the user is signedin

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import client_ID from '../credentials';
 
 class GoogleAuth extends Component {
+    state = { isSignedIn: null }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientID: client_ID,
+                clientId: client_ID,
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
             });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>I dont know if we are signed in</div>
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in!</div>
+        } else {
+            return <div>I am not signed in</div>
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>
+        return (
+            <div>{this.renderAuthButton()}</div>
+        )
     }
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                 <GoogleAuth />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
