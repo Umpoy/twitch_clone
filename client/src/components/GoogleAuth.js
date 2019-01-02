9bd5d81a421b1b5a07156a4942ae027b00043be2
Umpoy <iumpoy@yahoo.com>
2019-01-02 10:30:13 -0800

    connected mapstatetoprops

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
-    state = { isSignedIn: null }
+
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
@@ -13,7 +13,7 @@ class GoogleAuth extends Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
@@ -36,9 +36,9 @@ class GoogleAuth extends Component {
     }
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick={this.onSignOut} className="ui red google button">
                     <i className="google icon" />
@@ -62,4 +62,8 @@ class GoogleAuth extends Component {
     }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn }
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
