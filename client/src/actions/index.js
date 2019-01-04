68e3a70144bf7444877c2e9e841160fc0ac42305
Umpoy <iumpoy@yahoo.com>
2019-01-03 17:17:45 -0800

    able to fetch stream list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -29,25 +29,25 @@ export const createStream = formValues => async dispatch => {
 }
 
 export const fetchStreams = () => async dispatch => {
-    const response = await streams.get('/stream');
+    const response = await streams.get('/streams');
 
     dispatch({ type: FETCH_STREAMS, payload: response.data });
 }
 
 export const fetchStream = (id) => async dispatch => {
-    const response = await streams.get(`/stream/${id}`);
+    const response = await streams.get(`/streams/${id}`);
 
     dispatch({ type: FETCH_STREAM, payload: response.data });
 }
 
 export const deleteStream = (id) => async dispatch => {
-    await streams.delete(`/stream/${id}`);
+    await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/stream/${id}`, formValues);
+    const response = await streams.put(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data })
 }
