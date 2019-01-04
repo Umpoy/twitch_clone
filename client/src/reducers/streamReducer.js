75c1a585db79b01d8301801f8c1cda3f404cbe33
Umpoy <iumpoy@yahoo.com>
2019-01-03 16:59:10 -0800

    wired up stream reducer

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -6,16 +7,19 @@ import {
     DELETE_STREAM
 } from '../actions/types';
 
+
 export default (state = {}, action) => {
     switch (action.type) {
-        case FETCH_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
         case FETCH_STREAMS:
-            return { ...state, [action.payload.id]: action.payload }
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
+            return { ...state, [action.payload.id]: action.payload };
         case EDIT_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            return _.omit(state, action.payload)
         default:
             return state
     }
