234256e6ec895ddf5edb5fc110b28a7c48c8950f
Umpoy <iumpoy@yahoo.com>
2019-01-07 20:25:00 -0800

    added edit/delete buttons when signed in

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,10 +7,25 @@ class StreamList extends Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                </button>
+                    <button className="ui button negative">
+                        Delete
+                </button>
+                </div>
+            )
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
-                <div className="item" key={stream.id}>
+                <div className="item" key={stream.id}> {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -18,6 +33,7 @@ class StreamList extends Component {
                             {stream.description}
                         </div>
                     </div>
+
                 </div>
             )
         })
@@ -37,7 +53,10 @@ class StreamList extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams) }
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    }
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
