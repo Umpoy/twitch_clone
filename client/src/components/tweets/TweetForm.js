f544481c34f45f375534889ee9a4a12e3c590cc5
Umpoy <iumpoy@yahoo.com>
2019-01-16 15:50:03 -0800

    removed title

--- client/src/components/tweets/TweetForm.js
+++ client/src/components/tweets/TweetForm.js
@@ -32,7 +32,6 @@ class TweetForm extends Component {
     render() {
         return (
             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
             </form>
