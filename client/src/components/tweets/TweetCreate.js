ba58cc7dafc321d7391cf31a0fb077025835b916
Umpoy <iumpoy@yahoo.com>
2019-01-16 10:34:48 -0800

    refactored tweet creation

--- client/src/components/tweets/TweetCreate.js
+++ client/src/components/tweets/TweetCreate.js
@@ -1,30 +1,10 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createTweet } from '../../actions';
+import TweetForm from './TweetForm';
 
 class TweetCreate extends Component {
 
-    renderError({ error, touched }) {
-        if (touched && error) {
-            return (
-                <div className="ui error message">
-                    <div className="header">{error}</div>
-                </div>
-            )
-        }
-    }
-
-    renderInput = ({ input, label, meta }) => {
-        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
-        return (
-            <div className={className}>
-                <label>{label}</label>
-                <input {...input } autoComplete="off" />
-                {this.renderError(meta)}
-            </div>
-        )
-    }
 
     onSubmit = (formValues) => {
         console.log(formValues);
@@ -33,32 +13,12 @@ class TweetCreate extends Component {
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                <Field name="title" component={this.renderInput} label="Enter Title" />
-                <Field name="description" component={this.renderInput} label="Enter Description" />
-                <button className="ui button primary">Submit</button>
-            </form>
+            <div>
+                <h3>Create a Tweet</h3>
+                <TweetForm onSubmit={this.onSubmit} />
+            </div>
         )
     }
 }
 
-const validate = (formValues) => {
-    const errors = {};
-
-    if (!formValues.title) {
-        errors.title = 'You must enter a title'
-    }
-
-    if (!formValues.description) {
-        errors.description = 'You must enter a description'
-    }
-
-    return errors
-}
-
-const formWrapped = reduxForm({
-    form: 'tweetCreate',
-    validate
-})(TweetCreate);
-
-export default connect(null, { createTweet })(formWrapped);
+export default connect(null, { createTweet })(TweetCreate);
