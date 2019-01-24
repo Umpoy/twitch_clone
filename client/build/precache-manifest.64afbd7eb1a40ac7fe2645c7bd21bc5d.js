1ae7d4a22c64e1ae150f387e0c6a32b3c5eacf65
Umpoy <iumpoy@yahoo.com>
2019-01-24 09:30:48 -0800

    bundled oinkers client and ready to test in staging environment

---
+++ client/build/precache-manifest.64afbd7eb1a40ac7fe2645c7bd21bc5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e2ab9d4af02045a13dd",
+    "url": "/static/js/main.1e2ab9d4.chunk.js"
+  },
+  {
+    "revision": "cba7857f1d2e1f79a45e",
+    "url": "/static/js/1.cba7857f.chunk.js"
+  },
+  {
+    "revision": "1e2ab9d4af02045a13dd",
+    "url": "/static/css/main.79781a5e.chunk.css"
+  },
+  {
+    "revision": "282620f1cbd14b6792174cc16d783004",
+    "url": "/index.html"
+  }
+];
