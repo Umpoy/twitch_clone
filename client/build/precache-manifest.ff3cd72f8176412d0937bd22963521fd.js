9bfec78770ebda90d42b5ba6876e0e178c683a4d
Umpoy <iumpoy@yahoo.com>
2019-01-24 15:02:52 -0800

    rebundling

--- client/build/precache-manifest.ff3cd72f8176412d0937bd22963521fd.js
+++ client/build/precache-manifest.ff3cd72f8176412d0937bd22963521fd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "1e2ab9d4af02045a13dd",
-    "url": "/static/js/main.1e2ab9d4.chunk.js"
+    "revision": "7ac66088e5a9a5658911",
+    "url": "/static/js/main.7ac66088.chunk.js"
   },
   {
     "revision": "cba7857f1d2e1f79a45e",
     "url": "/static/js/1.cba7857f.chunk.js"
   },
   {
-    "revision": "1e2ab9d4af02045a13dd",
+    "revision": "7ac66088e5a9a5658911",
     "url": "/static/css/main.79781a5e.chunk.css"
   },
   {
-    "revision": "282620f1cbd14b6792174cc16d783004",
+    "revision": "4c83bb53ce2ed3ad5f61aec084e8489c",
     "url": "/index.html"
   }
 ];
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const port = 5002;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+
+app.listen(port, () => {
+    console.log('Port running on localhost:' + port)
+});
